408511cad99d8b68671f3ed4eb15dde07550ea53
mh-lamine <mlamins.ngom@gmail.com>
2024-07-06 20:14:58 +0200

    bring code coverage to 80%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,34 +2,27 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
 import router from "../app/Router.js";
+import store from "../__mocks__/store.js";
+import Bills from "../containers/Bills.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
+    beforeEach(() => {
       Object.defineProperty(window, "localStorage", {
         value: localStorageMock,
       });
-      window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-        })
-      );
+      window.localStorage.setItem("user", JSON.stringify({ type: "Employee" }));
       const root = document.createElement("div");
       root.setAttribute("id", "root");
       document.body.append(root);
       router();
       window.onNavigate(ROUTES_PATH.Bills);
-      await waitFor(() => screen.getByTestId("icon-window"));
-      const windowIcon = screen.getByTestId("icon-window");
-      //to-do write expect expression
     });
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills });
@@ -38,19 +31,71 @@ describe("Given I am connected as an employee", () => {
           /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
         )
         .map((a) => a.innerHTML);
-
-        console.log("Extracted dates:", dates);
-
-      // Define the sorting function for ascending order using Date objects
       const chrono = (a, b) => new Date(a) - new Date(b);
-
-      // Sort dates in ascending order
       const datesSorted = [...dates].sort(chrono);
 
-      console.log("Sorted dates:", datesSorted);
-
-      // Expect the dates array to be equal to the sorted dates array
       expect(dates).toEqual(datesSorted);
+    });
+
+    test("The newBill button should be clickable", async () => {
+      document.body.innerHTML = BillsUI({ data: bills });
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const newBillsInstance = new Bills({
+        document,
+        onNavigate,
+        store: store,
+        localStorage: window.localStorage,
+      });
+
+      const buttonNewBill = screen.getByTestId("btn-new-bill");
+      buttonNewBill.addEventListener(
+        "click",
+        newBillsInstance.handleClickNewBill
+      );
+
+      fireEvent.click(buttonNewBill);
+      await waitFor(() =>
+        expect(screen.getByText("Envoyer une note de frais")).toBeTruthy()
+      );
+    });
+
+    test("The eye icon should trigger modal display", async () => {
+      document.body.innerHTML = BillsUI({ data: bills });
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const newBillsInstance = new Bills({
+        document,
+        onNavigate,
+        store: store,
+        localStorage: window.localStorage,
+      });
+
+      $.fn.modal = jest.fn();
+
+      const iconEye = screen.getAllByTestId("icon-eye")[0];
+      iconEye.addEventListener("click", () =>
+        newBillsInstance.handleClickIconEye(iconEye)
+      );
+
+      fireEvent.click(iconEye);
+      expect($.fn.modal).toHaveBeenCalled();
+    });
+
+    test("Then getBills method should fetch and format bills correctly", async () => {
+      const newBillsInstance = new Bills({
+        document,
+        onNavigate: jest.fn(),
+        store: store,
+        localStorage: window.localStorage,
+      });
+
+      const bills = await newBillsInstance.getBills();
+      expect(bills.length).toBeGreaterThan(0);
+      expect(bills[0]).toHaveProperty("date");
+      expect(bills[0]).toHaveProperty("status");
     });
   });
 });
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,68 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+import { screen, fireEvent } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import store from "../__mocks__/store.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    let newBill;
+
+    beforeEach(() => {
+      document.body.innerHTML = NewBillUI();
+      window.localStorage = localStorageMock;
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({ type: "Employee", email: "test@test.com" })
+      );
+      newBill = new NewBill({
+        document,
+        onNavigate: (pathname) => {
+          document.body.innerHTML = pathname;
+        },
+        store: store,
+        localStorage: window.localStorage,
+      });
+    });
+
+    test("Then the page should render correctly", () => {
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+    });
+
+    describe("When I upload a file", () => {
+      test("Then handleChangeFile method should be called", () => {
+        const handleChangeFile = jest.fn(newBill.handleChangeFile);
+        const fileInput = screen.getByTestId("file");
+
+        fileInput.addEventListener("change", handleChangeFile);
+
+        fireEvent.change(fileInput, {
+          target: {
+            files: [new File(["sample"], "sample.png", { type: "image/png" })],
+          },
+        });
+
+        expect(handleChangeFile).toHaveBeenCalled();
+      });
+    });
+
+    describe("When I submit the form", () => {
+      test("Then handleSubmit method should be called and navigate to Bills page", () => {
+        const handleSubmit = jest.fn(newBill.handleSubmit);
+        newBill.handleSubmit = handleSubmit;
+        const form = screen.getByTestId("form-new-bill");
+
+        form.addEventListener("submit", handleSubmit);
+
+        fireEvent.submit(form);
+
+        expect(handleSubmit).toHaveBeenCalled();
+        expect(document.body.innerHTML).toEqual(ROUTES_PATH["Bills"]);
+      });
+    });
+  });
+});
